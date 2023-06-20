e7403e765f24f8ad9a08978c0d8ca74712da8ba6
ozy <giuseppe.fur96@gmail.com>
2023-06-20 11:33:53 +0200

    rename_columns added

--- main.py
+++ main.py
@@ -36,6 +36,20 @@ def remove_personal_info(df, personal_info):
     """
     return df.drop(*personal_info)
 
+def rename_columns(df: D, columns_to_rename: Sequence[T]) -> D:
+    """
+   Renames columns from dataframe.
+
+   :param dataframe df: dataframe where to rename
+   :param list columns_to_rename: list of tuples (old name, new name)
+   :return: dataframe with renamed columns
+   """
+    logger.debug(f"Data to be renamed: {columns_to_rename}")
+    for (old,new) in columns_to_rename:
+        df = df.withColumnRenamed(old, new)
+
+    return df
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--d1', type=str, required=True, help='path to the first dataset')
@@ -54,3 +68,5 @@ if __name__ == '__main__':
     df_full_clients = df_clients.join(df_fin, on=['id'], how='inner')
 
     df_full_clients = remove_personal_info(df=df_full_clients, personal_info=['first_name', 'last_name', 'cc_n'])
+
+    df_full_clients = rename_columns(df=df_full_clients, columns_to_rename=[('id', 'client_identifier'), ('btc_a', 'bitcoin_address'), ('cc_t', 'credit_card_type')])
