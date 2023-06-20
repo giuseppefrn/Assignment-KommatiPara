9a0f0c06761569b23a47245b632492dd357d33a3
ozy <giuseppe.fur96@gmail.com>
2023-06-20 11:26:24 +0200

    load_csv added

--- main.py
+++ main.py
@@ -13,6 +13,20 @@ os.makedirs("logs", exist_ok=True)
 handler = RotatingFileHandler("logs/kommatipara.log", maxBytes=200, backupCount=3)
 logger.addHandler(handler)
 
+def load_csv(filepath):
+    """
+    Load csv from a given filepath.
+
+    :param str filepath: path to the .csv file
+    :return: pyspark dataframe
+    """
+    if os.path.exists(filepath):
+        df = sc.read.option("header", "true").csv(filepath)
+        logger.debug(f"Data loaded: {filepath}")
+        #log dataset loaded
+        return df
+    logger.fatal(f"File doesn't exist: {filepath}")
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--d1', type=str, required=True, help='path to the first dataset')
@@ -24,3 +38,6 @@ if __name__ == '__main__':
     logger.debug(f"Arguments parsed: {opt.d1}, {opt.d2}, {opt.f}")
 
     sc = SparkSession.builder.master("local").appName("KommatiPara").getOrCreate()
+
+    df_clients = load_csv(opt.d1)
+    df_fin = load_csv(opt.d2)
