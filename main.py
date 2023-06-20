5147dea3ef5cbb69d893991591c7baed5008a28e
ozy <giuseppe.fur96@gmail.com>
2023-06-20 11:23:23 +0200

    logger added

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import argparse
+import os
 from pyspark.sql import SparkSession
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger("KommatiPara-Log")
+logger.setLevel(logging.DEBUG)
+
+os.makedirs("logs", exist_ok=True)
+
+handler = RotatingFileHandler("logs/kommatipara.log", maxBytes=200, backupCount=3)
+logger.addHandler(handler)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -8,5 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('--f','--list', nargs='+', help='Filter on Country', required=True)
 
     opt = parser.parse_args()
-    
+
+    logger.debug(f"Arguments parsed: {opt.d1}, {opt.d2}, {opt.f}")
+
     sc = SparkSession.builder.master("local").appName("KommatiPara").getOrCreate()
