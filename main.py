3d9902ae131ee5c1bb2bc0b24cb624bdd779737f
ozy <giuseppe.fur96@gmail.com>
2023-06-20 11:28:55 +0200

    filter_data added

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import argparse
 import os
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 
 import logging
 from logging.handlers import RotatingFileHandler
+
+from typing import TypeVar, Sequence
+
+T = TypeVar('T')
+C = TypeVar('C')
+D = TypeVar('D')
 
 logger = logging.getLogger("KommatiPara-Log")
 logger.setLevel(logging.DEBUG)
@@ -27,6 +34,18 @@ def load_csv(filepath):
         return df
     logger.fatal(f"File doesn't exist: {filepath}")
 
+def filter_data(df: D, filters: Sequence[T], colname: C) -> D:
+    """
+    Filters data given list of filters and a column name.
+
+    :param dataframe df: dataframe to be filtered
+    :param list filters: list of countries to keep
+    :param colname str colname: name of the column for the filter criteria
+    :return: filtered dataset
+    """
+    logger.debug(f"Filtering column: {colname} values: {filters}")
+    return df.filter(col(colname).isin(filters))
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--d1', type=str, required=True, help='path to the first dataset')
@@ -41,3 +60,5 @@ if __name__ == '__main__':
 
     df_clients = load_csv(opt.d1)
     df_fin = load_csv(opt.d2)
+
+    df_clients = filter_data(df=df_clients, filters=opt.f, colname='country')
